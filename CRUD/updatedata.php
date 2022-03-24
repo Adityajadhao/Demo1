880d43acaf982fad19a9c36a3e60ed74e11852d9
Aditya Jadhao <ajadhao@parkar.digital>
2022-03-24 12:52:28 +0530

    ALL CRUD OPS

--- CRUD/updatedata.php
+++ CRUD/updatedata.php
@@ -1,76 +1,80 @@
+<?php
+// $fn =$_GET['fn'];
+// $ln =$_GET['ln'];
+// $em =$_GET['em'];
+// $id=$_GET['id'];
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-<link rel="stylesheet" href="style.css">
+
+  <link rel="stylesheet" href="style.css">
 
 </head>
 
 <body>
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "registration_form";
+  $firstname = "";
+  $lastname = "";
+  $email = "";
+  $id = "";
+
+
+  if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $firstname = base64_decode(urldecode($_GET['fn']));
+    $lastname = base64_decode(urldecode($_GET['ln']));
+    $email = base64_decode(urldecode($_GET['em']));
+    $id = base64_decode(urldecode($_GET['id']));
+  }
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = $_POST['firstName'];
+    $lastname = $_POST['lastName'];
+    $email = $_POST['email'];
+    $id = $_POST['id'];
+
+
+    $sql = "UPDATE register SET firstname='$firstname',lastname='$lastname', email='$email' WHERE id='$id' ";
+    if ($conn->query($sql) === TRUE) {
+      echo "Record updated successfully";
+  ?>
+      <META HTTP-EQUIV="Refresh" CONTENT="1; URL=http://localhost/new/CRUD/viewdata.php">
+  <?php
+    } else {
+      echo "Error updating record: " . $conn->error;
+      $conn->close();
+    }
+  }
+  ?>
 
   <form action="updatedata.php" method="post">
-  <label for="column">Choose option to update:</label>
-  <select name="column" id="column">
-    <option value="firstname">Firstname</option>
-    <option value="lastname">Lastname</option>
-    <option value="email">Email</option>
-  </select>
-  <br><br>
-    Enter Input<br> <input type="text" name="txt" value="<?php if (isset($_POST['txt'])) {echo $_POST['txt'];} ?>"><br>
-    Enter Id<br> <input type="text" name="id" value="<?php if (isset($_POST['id'])) {echo $_POST['id'];} ?>"><br>
-    <button type="submit"> Submit </button><br>
+    <d iv class="container">
+      <h1>Update Data</h1>
+      <p>Please fill in details to update.</p>
+      <hr>
+      <label for="id"><b></b></label>
+      <input type="hidden" value="<?php echo $id ?>" name="id">
+      <label for="firstName"><b>FirstName</b></label>
+      <input type="text" value="<?php echo $firstname ?>" name="firstName">
+      <label for="lastName"><b>LastName</b></label>
+      <input type="text" value="<?php echo $lastname ?>" name="lastName" required>
 
+      <label for="email"><b>Email</b></label>
+      <input type="email" value="<?php echo $email ?>" name="email" required>
+      <button type="submit" class="registerbtn" name="submit">submit</button>
+  </form>
 </body>
 
 </html>
-
-<?php
-
-$choice = "";
-$input = "";
-$id = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-  $choice = $_POST["column"];
-  $input = $_POST["txt"];
-  $id= $_POST["id"];
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($input && $id )) {
-      echo "this field cannot be empty";
-    } else {
-      $servername = "localhost";
-      $username = "root";
-      $password = "";
-      $dbname = "registration_form";
-
-      // Create connection
-      $conn = new mysqli($servername, $username, $password, $dbname);
-      // Check connection
-      if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-      }
-      if ($choice=="firstname") {
-        $sql = "UPDATE register SET firstname='$input' WHERE id=$id";
-      }
-      elseif ($choice=="lastname") {
-        $sql = "UPDATE register SET lastname='$input' WHERE id=$id";
-      }
-      else {
-        $sql = "UPDATE register SET email='$input' WHERE id=$id";
-        
-      }
-
-      //$sql = "UPDATE register SET $input WHERE id=$id";
-
-      if ($conn->query($sql) === TRUE) {
-        echo "Record updated successfully";
-      } else {
-        echo "Error updating record: " . $conn->error;
-        $conn->close();
-      }
-    }
-  }
-}
-
-
-?>
--- CRUD/viewdata.php
+++ CRUD/viewdata.php
@@ -1,57 +1,86 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+  <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
+    td,
+    th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
 
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+  </style>
 </head>
-<body>
-    
-
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "registration_form";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT id, firstname, lastname, email FROM register";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  echo "<table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th></tr>";
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." "."</td><td>".$row["lastname"]." "."</td><td>".$row["email"]."</td></tr>";
+<script>
+  function checkdelete() {
+    return confirm('Confirm Delete?')
   }
-  echo "</table>";
-} else {
-  echo "0 results";
-}
-$conn->close();
+</script>
 
-?>
+<body>
+
+
+
+  <?php
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "registration_form";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  $count = '';
+  //$delete="<a href='deletedata.php' type='submit'>Delete </a><br>";
+  //$update="<a href='updatedata.php?fn=$row[firstname]&ln=$row[lastname]&em=$row[email]'' type='submit'>Update </a>";
+
+
+  $sql = "SELECT id, firstname, lastname, email FROM register";
+  $result = $conn->query($sql);
+
+
+  if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Update</th><th>Delete</th></tr>";
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+      echo "
+    <tr>
+    <td>" . ++$count . "</td>
+    <td>" . $row["firstname"] . " " . "</td>
+    <td>" . $row["lastname"] . " " . "</td>
+    <td>" . $row["email"] . "</td>"
+  ?>
+      <td><a href='updatedata.php?id=<?php echo urlencode(base64_encode($row['id'])); ?>&fn=<?php echo urlencode(base64_encode($row['firstname'])); ?>&ln=<?php echo urlencode(base64_encode($row['lastname'])); ?>&em=<?php echo urlencode(base64_encode($row['email'])); ?>'>Update </a></td>
+      <td><a href='delete.php?id=<?php echo urlencode(base64_encode($row['id'])); ?>' onclick='return checkdelete()'>Delete </a></td>
+      </tr>
+
+  <?php }
+    echo "</table>";
+  } else {
+    echo "0 results";
+    $conn->close();
+  }
+
+
+
+  ?>
+
+
 
 </body>
+
 </html>
